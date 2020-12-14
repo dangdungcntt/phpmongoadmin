57630664703939b17a467f152dd948b2d16c9e38
Dung Nguyen Dang <dangdungcntt@gmail.com>
2020-12-14 14:14:28 +0700

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@push('content')
+    <div style="background: red;height: 100px"></div>
+@endpush
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/docs.css') }}">
+    @livewireStyles
+</head>
+
+<body>
+
+<nav class="bd-subnavbar py-2" aria-label="Secondary navigation">
+    <div class="container-fluid d-flex align-items-md-center">
+        PHP MONGO ADMIN
+    </div>
+</nav>
+
+<div class="container-fluid my-md-4 bd-layout">
+    @include('layouts.sidebar')
+    <div>
+        @stack('content')
+    </div>
+</div>
+
+@include('layouts.footer')
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"
+        integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"
+        integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj"
+        crossorigin="anonymous"></script>
+@livewireScripts
+</body>
+</html>
