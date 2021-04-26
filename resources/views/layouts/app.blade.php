aec4ad166a2b625d7a48828c5d9dab51db5f565c
Dung Nguyen Dang <dangdungcntt@gmail.com>
2021-04-26 11:34:57 +0700

    Update boostrap5 beta 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield('page_title', config('app.name'))</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}?v={{ config('app.version') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}?v={{ config('app.version') }}">
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
     <link rel="stylesheet" href="{{ asset('css/docs-sidebar.css') }}">
     @livewireStyles
@@ -45,8 +44,8 @@
     </main>
 </div>
 
-<script src="{{ asset('js/popper.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/popper.min.js') }}?v={{ config('app.version') }}"></script>
+<script src="{{ asset('js/bootstrap.min.js') }}?v={{ config('app.version') }}"></script>
 @livewireScripts
 @stack('scripts')
 </body>
